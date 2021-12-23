057a0bc1ae234ddff55d2f0f6105bd1f69ecd3cc
dalfonso.barrera <dalfonso.barrera@udea.edu.co>
2021-12-23 13:58:08 -0500

    clase

--- app/src/main/java/com/example/dalfonso/bookproject/MainActivity.kt
+++ app/src/main/java/com/example/dalfonso/bookproject/MainActivity.kt
@@ -1,13 +1,9 @@
 package com.example.dalfonso.bookproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.dalfonso.bookproject.databinding.ActivityMainBinding
-import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
 
