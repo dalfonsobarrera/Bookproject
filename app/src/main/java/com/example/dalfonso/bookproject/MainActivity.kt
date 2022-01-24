e405b51ea77e18a85c8841223d680c7ed6cf3884
dalfonso.barrera <dalfonso.barrera@udea.edu.co>
2022-01-24 15:14:30 -0500

    h

--- app/src/main/java/com/example/dalfonso/bookproject/MainActivity.kt
+++ app/src/main/java/com/example/dalfonso/bookproject/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.dalfonso.bookproject
 
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.widget.DatePicker
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.dalfonso.bookproject.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mainBinding : ActivityMainBinding
+    private var cal = Calendar.getInstance()
+    private var publicationDate= ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,11 +23,31 @@ class MainActivity : AppCompatActivity() {
 
         // val nameBookEditText : EditText = findViewById(R.id.name_book_edit_text)
 
-        var numero : Int? = null
+        val dateSetListener = DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, month)
+            cal.set(Calendar.DAY_OF_YEAR, dayOfMonth)
 
-        numero = null
+            val format = "MM/dd/yyyy"
+            val simpleDateFormat = SimpleDateFormat(format, Locale.US)
+            publicationDate = simpleDateFormat.format(cal.time).toString()
+            mainBinding.publicationDateButton.text = publicationDate
+
+
+        }
 
         with(mainBinding) {
+
+            publicationDateButton.setOnClickListener {
+                DatePickerDialog(
+                    this@MainActivity,
+                    dateSetListener,
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)
+                ).show()
+            }
+
             saveButton.setOnClickListener {
 
                 if (nameBookEditText.text.isEmpty() ||
@@ -33,12 +59,31 @@ class MainActivity : AppCompatActivity() {
                         Toast.LENGTH_LONG).show()
                 }   else{
 
-                val nameBook: String = nameBookEditText.text.toString()
-                val author = nameAuthorEditText.text.toString()
-                val pages = pagesEditText.text .toString().toInt()
-                val abstract = abstractEditText.text.toString()
+                    val nameBook: String = nameBookEditText.text.toString()
+                    val author = nameAuthorEditText.text.toString()
+                    val pages = pagesEditText.text .toString().toInt()
+                    val abstract = abstractEditText.text.toString()
 
-                infoTextView.text = getString(R.string.info,nameBook, author, pages, abstract)
+                    var genre = ""
+
+                    if (suspenseCheckBox.isChecked) genre = " Suspenso "
+                    if (terrorCheckBox.isChecked) genre += " Terror "
+                    if (infantileCheckBox.isChecked) genre += " Infantil "
+                    if (fictionCheckBox.isChecked) genre += " Ficción "
+
+                    var score = when {
+                        oneRadioButton.isChecked -> 1
+                        twoRadioButton.isChecked -> 2
+                        threeRadioButton.isChecked -> 3
+                        fourRadioButton.isChecked -> 4
+                        else -> 5
+                    }
+
+
+
+
+
+                infoTextView.text = getString(R.string.info,nameBook, author, pages, abstract, genre, score)
                 }
             }
 
